96df51c4a1bccb61586fb5ed66feafaca62fe203
mr-j-shah <shahjinay02@gmail.com>
2022-10-28 19:06:45 +0530

    First Init

---
+++ read-data-get-method.php
@@ -0,0 +1,32 @@
+<?php 
+    header("Access-Control-Allow-Headers: Authorization, Content-Type");
+    header("Access-Control-Allow-Origin: *");
+    header('content-type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+    $servername="localhost";
+    $user="root";
+    $password="";
+    $db="jinay";
+    $conn = new mysqli($servername, $user, $password,$db);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        $loaninfo = array();    
+        if($_SERVER["REQUEST_METHOD"] == "GET")
+        {
+	        $sql="SELECT * FROM flutter";
+
+            $result = $conn->query($sql) or die("Error in Selecting " . mysqli_error($conn));
+	        if ($result) {    
+                while($row=$result->fetch_assoc()){
+			        $loaninfo[]=$row;
+		        }
+	        } else {
+		        $loaninfo[] ="Sorry";	
+	        }
+	        echo json_encode($loaninfo);
+        }
+        $conn->close();
+    }
+?>
